901f2f9ad1f10c8e764eacf12fcb0ccd0877246c
Huzaifa Arain <huzaifa.itgroup@gmail.com>
2015-12-28 23:57:50 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,42 @@
-loadMessages(<?php echo $post->id; ?>,4);
 
-	window.mutexLocalMessages = "unlock";
+	var x = "Your identical id";
+	var y = "Starting From";
+ 	loadMessages(x,y);
+
+	window.mutexLocalMessages = "unlock"; 
+	/**
+	 * This is global variable attached to window object, its purpose is to sent one request at a time. so the logic is
+	 * if the variable value is unlock then request can proceed otherwise if its lock then request have to wait until its unlock
+	*/
+	
+	// LoadMessages() will run every 1 seconds interval
 	setInterval(function(){
-		
+		// if resource is unlock
 		if(window.mutexLocalMessages == "unlock")
 		{
+			// lock the resource so that no other request can in
 			window.mutexLocalMessages = "lock";
-			var lastid = $(".timeline-messages .msg-time-chat:last-child").data("id");
-			loadMessages(<?php echo $post->id; ?>,lastid);
+			var lastid = $(".timeline-messages .msg-time-chat:last-child").data("id"); // the id of last message shown in message list
+			loadMessages(x,lastid);
 		}
 	}, 1000);
 	
 	
  $("#cnrForm").on("submit",function(e){
-        e.preventDefault();
+        e.preventDefault(); 
         e.stopPropagation();
         $("[name='message']").prop("readonly",true);
         var lastid = $(".timeline-messages .msg-time-chat:last-child").data("id");
-        console.log(lastid);
         var data = $(this).serializeArray();
         var ajaxObject = $.post("<?php echo base_url('user'); ?>",data,null,"json");
-        console.log(ajaxObject);
         ajaxObject.done(function(data){
             if(data == true)
             {
+            	// status is true mean all set but reload request is only required if its unlock
             	if(window.mutexLocalMessages == "unlock")
             	{
+            		// lock the resource
+            		window.mutexLocalMessages == "lock";
             		loadMessages(<?php echo $post->id; ?>,lastid);
                 }
             	$("[name='message']").prop("readonly",false);
